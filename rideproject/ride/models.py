09d4ec15f8d94eda2fdf9d466809e7e8c9fc76a2
Aswin <aswinjikz071@gmail.com>
2023-11-19 10:33:10 +0530

    ride model

--- rideproject/ride/models.py
+++ rideproject/ride/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from accounts.models import Account
 
-# Create your models here.
+class Ride(models.Model):
+    rider = models.ForeignKey(Account, on_delete=models.CASCADE, related_name='rides_as_rider')
+    driver = models.ForeignKey(Account, on_delete=models.CASCADE, related_name='rides_as_driver')
+    pickup_location = models.CharField(max_length=100)
+    dropoff_location = models.CharField(max_length=100)
+    STATUS_CHOICES = [
+        ('requested', 'Requested'),
+        ('started', 'Started'),
+        ('completed', 'Completed'),
+        ('cancelled', 'Cancelled'),
+    ]
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='requested')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
